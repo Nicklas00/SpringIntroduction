b3a66a0e54ce16afe3f01db9e6ad632c6b9238be
Nicklas Hansen <nick553h@stud.kea.dk>
2021-09-27 15:28:59 +0200

    opgave 1-3 i intoduction til spring

---
+++ src/main/java/com/example/springintroduction/HelloSpring.java
@@ -0,0 +1,68 @@
+package com.example.springintroduction;
+import com.example.springintroduction.services.DateCalc;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class HelloSpring {
+  /*@GetMapping("/first-mapping")
+  public String firstMapping(){
+    return "<b>Hello World<b>";
+  }
+
+  @GetMapping("/second-mapping")
+  public String secondMapping(){
+    return "This is the second mapping";
+  }
+
+  @GetMapping("/")
+  public String index(){
+    return "Welcome to my Spring Application";
+  }
+
+  @GetMapping("/parameter")
+  public String parameter(@RequestParam String input){
+    //For this to work:
+    //localhost:8080/parameter?input=HelloWorld
+    return input + " " + "end of string";
+  }
+
+   */
+
+
+  @GetMapping("/greeting")
+  public String greeting(){
+    return "Greetings, have a nice day";
+  }
+
+  @GetMapping("/parameter")
+  public String echo(@RequestParam String input){
+    String yourString = "<p>Dette er din string^^</p>";
+    return "<h>" + input + "</h>" + " " + yourString;
+
+  }
+
+  @GetMapping("/erdetfredag")
+  public String erDetFredag(){
+    DateCalc dateCalc = new DateCalc();
+
+    int date = dateCalc.erDetFredag();
+    String dagenIdag = "";
+    if(date == 1){
+      dagenIdag = "Det er mandag";
+    }else if(date == 2){
+      dagenIdag = "Det er tirsdag";
+    }else if(date == 3){
+      dagenIdag = "Det er onsdag";
+    }else if(date == 4){
+      dagenIdag = "Det er torsdag";
+    }else if(date == 5){
+      dagenIdag = "DET ER FREDAG MINE GUTTER";
+    }
+    return dagenIdag;
+  }
+
+}
