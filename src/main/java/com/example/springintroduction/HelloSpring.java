b11e0ecaf315a79abc4d9cadece12c0987bbbb99
Nicklas Hansen <nick553h@stud.kea.dk>
2021-09-28 14:53:06 +0200

    løsning til de 3 opgaver

--- src/main/java/com/example/springintroduction/HelloSpring.java
+++ src/main/java/com/example/springintroduction/HelloSpring.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 public class HelloSpring {
   /*@GetMapping("/first-mapping")
